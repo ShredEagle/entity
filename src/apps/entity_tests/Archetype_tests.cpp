b70f8e058cb9ff4067f274cb612109d71a30dae9
Francois Poizat <francois.poizat@gmail.com>
2023-05-23 13:35:24 +0200

    Adds tests to check edge case that could threaten archetype and query integrity

---
+++ src/apps/entity_tests/Archetype_tests.cpp
@@ -0,0 +1,53 @@
+#include "catch.hpp"
+
+#include "Components_helpers.h" 
+
+#include <entity/Entity.h>
+#include <entity/Query.h>
+#include <entity/EntityManager.h>
+
+using namespace ad;
+using namespace ad::ent;
+
+
+SCENARIO("Archetype should be stable")
+{
+    GIVEN("An entity manager with two entities")
+    {
+        EntityManager world;
+        Handle<Entity> h1 = world.addEntity();
+        Handle<Entity> h2 = world.addEntity();
+
+        GIVEN("An archetype with ComponentA and ComponentB containing h1 and h2")
+        {
+            Query<ComponentA, ComponentB> q{world};
+            const std::string str{"rrpp58"};
+            {
+                Phase init;
+                h1.get(init)
+                    ->add<ComponentA>({5.8})
+                    .add<ComponentB>({str})
+                ;
+                h2.get(init)
+                    ->add<ComponentA>({5.9})
+                    .add<ComponentB>({str})
+                ;
+            }
+
+            WHEN("Adding ComponentA should not affect the [ComponentA, ComponentB] Archetype")
+            {
+                {
+                    Phase modify;
+                    h2.get(modify)->add<ComponentA>({6.8});
+                    h1.get(modify)->add<ComponentA>({6.9});
+                }
+
+                THEN("The archetype is valid")
+                {
+                    q.verifyEntityIds();
+                }
+            }
+        }
+    }
+}
+
